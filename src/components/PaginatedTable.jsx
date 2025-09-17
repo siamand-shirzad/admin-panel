0b9940bf1b56fcc1955879bf3c0d3fb377a52659
siamand <siamand1381@gmail.com>
2025-09-17 20:26:57 +0330

    style(paginatedTable): styling pagination

--- src/components/PaginatedTable.jsx
+++ src/components/PaginatedTable.jsx
@@ -1,27 +1,25 @@
 import { useEffect, useState } from 'react';
 
-
-const numOfPage = 2
+const numOfPage = 2;
 const PaginatedTable = ({ data, dataInfo, additionField }) => {
-  const [tableData, setTableData] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [pages, setPages] = useState([])
-  const [pageCount, setPageCount] =useState(1)
-  useEffect(() => {
-    let pCount = Math.ceil( data.length / numOfPage)
-    setPageCount(pCount)
-    let pArr = []
-    for (let i = 1; i <= pCount; i++) pArr = [...pArr, i]
-    setPages(pArr)
-  }, [])
-  
-  useEffect(() => {
-    let start = (currentPage*numOfPage) -numOfPage;
-    let end = (currentPage*numOfPage);
-    setTableData(data.slice(start,end))
-  
-  }, [currentPage])
-  
+	const [tableData, setTableData] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pages, setPages] = useState([]);
+	const [pageCount, setPageCount] = useState(1);
+	useEffect(() => {
+		let pCount = Math.ceil(data.length / numOfPage);
+		setPageCount(pCount);
+		let pArr = [];
+		for (let i = 1; i <= pCount; i++) pArr = [...pArr, i];
+		setPages(pArr);
+	}, []);
+
+	useEffect(() => {
+		let start = currentPage * numOfPage - numOfPage;
+		let end = currentPage * numOfPage;
+		setTableData(data.slice(start, end));
+	}, [currentPage]);
+
 	return (
 		<div>
 			<table className="table table-responsive text-center table-hover table-bordered">
@@ -30,7 +28,7 @@ const PaginatedTable = ({ data, dataInfo, additionField }) => {
 						{dataInfo.map(i => (
 							<th key={i.field}>{i.title}</th>
 						))}
-						{additionField ? (<th>{additionField.title}</th>) : null}
+						{additionField ? <th>{additionField.title}</th> : null}
 					</tr>
 				</thead>
 				<tbody>
@@ -39,36 +37,40 @@ const PaginatedTable = ({ data, dataInfo, additionField }) => {
 							{dataInfo.map(i => (
 								<td key={i.field + '_' + d.id}>{d[i.field]}</td>
 							))}
-							{additionField ? (<td>{additionField.elements(d.id)}</td>) : null}
+							{additionField ? <td>{additionField.elements(d.id)}</td> : null}
 						</tr>
 					))}
 				</tbody>
 			</table>
-      			<nav aria-label="Page navigation example" className="d-flex justify-content-center">
+			<nav aria-label="Page navigation example" className="d-flex justify-content-center">
 				<ul className="pagination dir_ltr">
 					<li className="page-item pointer">
-						<span className={`page-link pointer ${currentPage == 1 ? "disabled": ""}`}  aria-label="Previous" onClick={()=>setCurrentPage(currentPage-1)}>
+						<span
+							className={`page-link pointer ${currentPage == 1 ? 'disabled' : ''}`}
+							aria-label="Previous"
+							onClick={() => setCurrentPage(currentPage - 1)}>
 							<span aria-hidden="true">&raquo;</span>
 						</span>
 					</li>
-          {
-            pages.map(page=>(
-              
-					<li key={page} className="page-item pointer">
-						<span className="page-link" onClick={()=>setCurrentPage(page)}>
-							{page}
-						</span>
-					</li>
-            ))
-          }
+					{pages.map(page => (
+						<li key={page} className="page-item ">
+							<div
+								className={`page-link pointer  ${currentPage == page ? 'alert alert-success' : ''}`}
+								onClick={() => setCurrentPage(page)}>
+								{page}
+							</div>
+						</li>
+					))}
 					<li className="page-item">
-						<span className={`page-link pointer ${currentPage == pageCount ? "disabled": ""}`}  aria-label="Next" onClick={()=>setCurrentPage(currentPage+1)}>
+						<span
+							className={`page-link pointer ${currentPage == pageCount ? 'disabled' : ''}`}
+							aria-label="Next"
+							onClick={() => setCurrentPage(currentPage + 1)}>
 							<span aria-hidden="true">&laquo;</span>
 						</span>
 					</li>
 				</ul>
 			</nav>
-
 		</div>
 	);
 };
