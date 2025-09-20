c414b3a4319a3f529e81d4ca45baaa7b8429e3a8
siamand <siamand1381@gmail.com>
2025-09-20 16:52:15 +0330

    feat: implementing react router

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -3,6 +3,7 @@ import Category from './category/Category';
 import { useContext } from 'react';
 import { AdminContext } from '../context/AdminLayoutContext';
 import Product from './product/Product';
+import { Route, Routes } from 'react-router-dom';
 
 const Content = () => {
   	const { showSidebar } = useContext(AdminContext);
@@ -11,9 +12,13 @@ const Content = () => {
 		<section
 			id="content_section"
 			className={`bg-light py-2 px-3 ${showSidebar ? 'with_sidebar' : null}`}>
-			{/* <Dashbord/> */}
-			<Category />
-			{/* <Product/> */}
+				<Routes>
+					<Route path='/' element={<Dashbord/>} />
+					<Route path='/categories' element={<Category />} />
+					<Route path='/products' element={<Product/>} />
+					<Route path='*' element={<Dashbord/>} />
+				</Routes>
+			
 		</section>
 	);
 };
