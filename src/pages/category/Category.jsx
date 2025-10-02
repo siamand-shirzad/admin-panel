8941fde6f1dd655a26e34cac79d1af7d10c61046
siamand <siamand1381@gmail.com>
2025-10-02 19:20:45 +0330

    feat (category): implementing AddAtributes component

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import CategoryTable from './CategoryTable';
 import AddCategory from './AddCategory';
+import AddAttributes from './AddAttributes';
 
 const Category = () => {
 	return (
 		<div id="manage_product_category" className="manage_product_category main_section ">
 			<h4 className="text-center my-3">مدیریت دسته بندی محصولات</h4>
 			<CategoryTable />
+			<AddAttributes/>
 		</div>
 	);
 };
