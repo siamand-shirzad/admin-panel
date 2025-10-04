1b96d4c08ba477085be2f7b122d5751bacf47782
siamand <siamand1381@gmail.com>
2025-10-04 17:33:00 +0330

    feat : implementing form components

---
+++ src/components/form/Switch.jsx
@@ -0,0 +1,16 @@
+import { FastField } from 'formik';
+
+const Switch = ({name, label}) => {
+    return (
+        <div className="form-check form-switch dir_ltr ">
+        <FastField
+          className="form-check-input"
+          type="checkbox"
+          name={name}
+        />
+        <label className="form-check-label ">{label}</label>
+      </div>
+    );
+}
+
+export default Switch;
