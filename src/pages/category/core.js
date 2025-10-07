5f50c2bb7268e016151e87cfc3373d8d106985c3
siamand <siamand1381@gmail.com>
2025-10-07 09:43:20 +0330

    refactor: move Formik logic to core.js

---
+++ src/pages/category/core.js
@@ -0,0 +1,51 @@
+import { Alert } from "bootstrap";
+import { addNewCategoryService } from "../../services/category";
+import * as Yup from 'yup';
+
+
+export const initialValues = {
+	parent_id: '',
+	title: '',
+	description: '',
+	image: null,
+	is_active: true,
+	show_in_menu: true
+};
+
+export const onSubmit = async (values, actions, setForceRender) => {
+	try {
+		values = {
+			...values,
+			is_active: values.is_active ? 1 : 0,
+			show_in_menu: values.show_in_menu ? 1 : 0
+		};
+		const res = await addNewCategoryService(values);
+		if (res.status == 201) {
+			Alert('رکورد ثبت شد', 'عملیات با موفقیت انجام شد', 'success');
+			actions.resetForm();
+			setForceRender(prev => prev + 1);
+		}
+	} catch (error) {}
+};
+
+export const validationSchema = Yup.object({
+	parent_id: Yup.number(),
+	title: Yup.string()
+		.required('لطفا این قسمت را پر کنید')
+		.matches(/^[\u0600-\u06FF\sa-zA-Z0-9@!%$?&]+$/, 'فقط از حروف و اعداد استفاده شود'),
+	description: Yup.string().matches(
+		/^[\u0600-\u06FF\sa-zA-Z0-9@!%$?&]+$/,
+		'فقط از حروف و اعداد استفاده شود'
+	),
+	image: Yup.mixed()
+		.nullable() // اجازه میده مقدار null باشه
+		.notRequired() // یعنی اجباری نیست
+		.test('filesize', 'حجم فایل نمیتواند بیشتر 500 کیلوبایت باشد', value =>
+			!value ? true : value.size <= 500 * 1024
+		)
+		.test('format', 'فرمت فایل باید jpg باشد', value =>
+			!value ? true : value.type === 'image/jpeg'
+		),
+	is_active: Yup.boolean(),
+	show_in_menu: Yup.boolean()
+});
